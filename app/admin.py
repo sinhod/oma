2c202574ed413a1e8b13b879bf6639266137f9d9
Sini Hodju <sinihodju@student.careeria.fi>
2023-12-08 00:59:05 +0200

    models, admin ja tietokanta muutoksia

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from app.models import Work, Installation, Contact
 
 # Register your models here.
+
+@admin.register(Work)
+class WorkAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Installation)
+class InstallationAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    pass
--- app/models.py
+++ app/models.py
@@ -1,32 +1,40 @@
 from django.db import models
 
 # Teosten tiedot
-class work(models.Model):
+class Work(models.Model):
     name=models.CharField(max_length=50, default="teoksen nimi")
     year=models.IntegerField(default=0)
     media=models.CharField(max_length=100, default="tekniikka")
     imageLink_1=models.CharField(max_length=2090, default="kuvalinkki")
+    def __str__(self):
+        return f"{self.name} {self.year}"
 
 # Installaatioiden tiedot
 class Installation(models.Model):
     installationName=models.CharField(max_length=50, default="installaation nimi")
-    collaboration=models.CharField(max_length=100, default="")
+    collaboration=models.CharField(max_length=100, default="", null=True, blank=True)
     year=models.IntegerField(default=0)
     media=models.CharField(max_length=100, default="tekniikka")
-    imageLink_1=models.CharField(max_length=2090, default="")
-    imageLink_2=models.CharField(max_length=2090, default="")
-    imageLink_3=models.CharField(max_length=2090, default="")
-    imageLink_4=models.CharField(max_length=2090, default="")
-    videoLink=models.CharField(max_length=2090, default="")
-    instagramLink=models.CharField(max_length=2090, default="")
+    imageLink_1=models.CharField(max_length=2090, default="", null=True, blank=True)
+    imageLink_2=models.CharField(max_length=2090, default="", null=True, blank=True)
+    imageLink_3=models.CharField(max_length=2090, default="", null=True, blank=True)
+    imageLink_4=models.CharField(max_length=2090, default="", null=True, blank=True)
+    videoLink=models.CharField(max_length=2090, default="", null=True, blank=True)
+    instagramLink=models.CharField(max_length=2090, default="", null=True, blank=True)
+    def __str__(self):
+        return f"{self.installationName}"
 
-class contact(models.Model):
+class Contact(models.Model):
     # Yhteydenottajan tiedot ja viestin sisältö
     sendersName=models.CharField(max_length=50, default="lähettäjän nimi")
     sendersMessage=models.CharField(max_length=1000, default="lähettäjän viesti")
     sendersEmail=models.CharField(max_length=50, default="lähettäjän email")
-    sendersPhonenumber=models.IntegerField(default="lähettäjän puh.nro")
+    sendersPhonenumber=models.CharField(max_length=50, default="lähettäjän puh.nro")
     # Kuittausviesti yhteydenottajalle
     messageForSender=models.CharField(max_length=1000, default="")
     # Ilmoitus tulleesta viestistä
-    messageForRecipient=models.CharField(max_length=1000, default="")
+    message=models.CharField(max_length=1000, default="")
+    # Oma email
+    email=models.CharField(max_length=50, default="vastaanottajan email")
+    def __str__(self):
+        return f"{self.sendersName}"
