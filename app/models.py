db10c93daa6dab6cab53ff7ffd11e3c4c462a4b5
Sini Hodju <sinihodju@student.careeria.fi>
2023-12-07 21:51:41 +0200

    navigointinapit

--- app/models.py
+++ app/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-# Maalaukset, kollaasit, piirustukset yms.
+# Teosten tiedot
 class work(models.Model):
     name=models.CharField(max_length=50, default="")
     year=models.IntegerField(default=0)
     media=models.CharField(max_length=100, default="")
     imageLink_1=models.CharField(max_length=2090, default="")
 
-# Installaatiot
+# Installaatioiden tiedot
 class Installation(models.Model):
     installationName=models.CharField(max_length=50, default="")
     year=models.IntegerField(default=0)
@@ -25,7 +25,7 @@ class contact(models.Model):
     sendersMessage=models.CharField(max_length=1000, default="")
     sendersName=models.CharField(max_length=50, default="")
     sendersEmail=models.CharField(max_length=50, default="")
-    sendersPhonenumber=models.IntegerField(max_length=20, default="")
+    sendersPhonenumber=models.IntegerField(default="")
     # Kuittausviesti yhteydenottajalle
     messageForSender=models.CharField(max_length=1000, default="")
     # Ilmoitus tulleesta viestistä
